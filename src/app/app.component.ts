5ac2b7cecba1fb1354eee8229e71ca08d68bdf83
Gee Chong Shek <geeshek95@outlook.com>
2024-02-01 13:48:28 +0000

    Changes to app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div style="text-align:center">
+  <h1>
+    'Welcome to {{pageTitle}}!!
+  </h1>
+  <div>My First Component</div>
+  </div>
+  `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
